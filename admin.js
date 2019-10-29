670ed1264f49e2268096b8eefd418ad8b11a9b01
xLazaki <boss_12389@hotmail.com>
2019-10-29 11:50:34 +0700

    home

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-function refresh(){
+function refresh() {
     axios({
         method: 'post',
         url: 'https://api.pattanachai.xyz/timeslots',
@@ -11,23 +11,25 @@ function refresh(){
         show();
     }).catch((err) => {
         console.log(err.toString());
-    })  
+    })
 }
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
+
 function checktocancel(x, k) {
     if (timeslots[k].reserved == true) {
         if (confirm("confirm to cancel") == true) {
@@ -36,11 +38,12 @@ function checktocancel(x, k) {
             axios({
                 method: 'patch',
                 url: 'https://api.pattanachai.xyz:/free',
-                headers: {authorization:getCookie(getCookie('user'))
+                headers: {
+                    authorization: getCookie(getCookie('user'))
                 },
                 data: [{ room: 'testRoom', date: 'testDate', slot: k }],
             }).then((response) => {
-                if (response.data.success==true) {
+                if (response.data.success == true) {
                     console.log(response.data.reservationDetails);
                     refresh();
                 }
@@ -54,27 +57,29 @@ function checktocancel(x, k) {
         }
     }
 }
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
+
 function status(x, i) {
     if (timeslots[i].reserved == true) {
         document.getElementById(x).style.backgroundColor = "red";
         document.getElementsByClassName('Reserve')[i].style.display = 'none';
         document.getElementsByClassName('cancel')[i].style.display = 'block';
-        document.getElementsByClassName('user')[i].innerHTML = timeslots[i].reservervationID
+        document.getElementsByClassName('user')[i].innerHTML = timeslots[i].reserver;
     } else {
         document.getElementById(x).style.backgroundColor = "greenyellow";
         document.getElementsByClassName('cancel')[i].style.display = 'none';
@@ -104,43 +109,43 @@ function reserve_choose(i) {
         alert('This time has been reserve')
     }
 }
+
 function submit() {
-        var datasend=[];
-        for (var i = 0; i < 20; i++) {
-            if(a[i] == 'chosen'){
-                datasend.push({reserver:getCookie('user'),room: "testRoom",date: "testDate",slot: i.toString(10)})
-            }
-        }
-        if (datasend.length==0){
-            alert("Please select")
-        }else{
-            axios({
-                method: 'post', 
-                url: 'https://api.pattanachai.xyz:/reserve',
-                headers: {
-                    Authorization: getCookie(getCookie('user'))
-                },
-                data: datasend,
-                }).then((response)=>{
-                    if(response.data.success==true){
-                        alert('reserve successfully');
-                        refresh();
-                    }
-                    else{
-                        alert("can't reserve");
-                    
-                    }
-                }).catch((err)=>{
-                    if(err.response.status==401){
-                        //not logged in (didn't attach token in the header)
-                        alert('please login');
-                        location.replace('login.html');
-                    }
-                })
+    var datasend = [];
+    for (var i = 0; i < 20; i++) {
+        if (a[i] == 'chosen') {
+            datasend.push({ reserver: getCookie('user'), room: "testRoom", date: "testDate", slot: i.toString(10) })
         }
     }
+    if (datasend.length == 0) {
+        alert("Please select")
+    } else {
+        axios({
+            method: 'post',
+            url: 'https://api.pattanachai.xyz:/reserve',
+            headers: {
+                Authorization: getCookie(getCookie('user'))
+            },
+            data: datasend,
+        }).then((response) => {
+            if (response.data.success == true) {
+                alert('reserve successfully');
+                refresh();
+            } else {
+                alert("can't reserve");
 
-var a = ['','','','','','','','','','','','','','','','','','','','']
+            }
+        }).catch((err) => {
+            if (err.response.status == 401) {
+                //not logged in (didn't attach token in the header)
+                alert('please login');
+                location.replace('login.html');
+            }
+        })
+    }
+}
+
+var a = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 var id_status = ["index0_can", "index1_can", "index2_can", "index3_can", "index4_can", "index5_can", "index6_can", "index7_can", "index8_can", "index9_can",
     "index10_can", "index11_can", "index12_can", "index13_can", "index14_can", "index15_can", "index16_can", "index17_can", "index18_can", "index19_can"
 ];
