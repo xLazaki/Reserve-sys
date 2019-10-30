32a7218d61baad5fffe150b0944c0378776fcc4f
xLazaki <boss_12389@hotmail.com>
2019-10-30 20:56:06 +0700

    add room and date

--- admin.js
+++ admin.js
@@ -1,62 +1,68 @@
-function refresh() {
+function refresh(x, l) {
     axios({
         method: 'post',
         url: 'https://api.pattanachai.xyz/timeslots',
-        data: { room: 'testRoom', date: 'testDate' },
+        data: { room: l, date: x },
     }).then((response) => {
         timeslots = response.data;
+        console.log(response.data)
         for (var i = 0; i < 20; i++) {
             a[i] = timeslots[i].reserved;
         }
         show();
     }).catch((err) => {
-        console.log(err.toString());EAD
-    })  
+        console.log(err.toString());
+    })
 }
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
+
 function checktocancel(x, k) {
+    var y = document.getElementById('date').value;
     if (timeslots[k].reserved == true) {
         if (confirm("confirm to cancel") == true) {
             document.getElementById(x).style.backgroundColor = "red";
             timeslots[k].reserved = false;
             axios({
-                method: 'patch', 
+                method: 'patch',
                 url: 'https://api.pattanachai.xyz/free',
                 headers: {
                     Authorization: getCookie(getCookie('user'))
                 },
-                data: [{ room: 'testRoom', date: 'testDate', slot: k }],
-                }).then((response)=>{
-                if(response.data.success){
-                    refresh();
+                data: [{ room: document.getElementById('Room'), date: y, slot: k }],
+            }).then((response) => {
+                if (response.data.success) {
+                    alert('Success')
+                } else {
+                    alert('Please select date')
+                        //can't free
+                        // response.data.fail <-- array ของการจองที่ยกเลิกไม่ได้ทั้งหมด
                 }
-                else{
-                    //can't free
-                    // response.data.fail <-- array ของการจองที่ยกเลิกไม่ได้ทั้งหมด
+                refresh(y);
+            }).catch((err) => {
+                if (err.response.status == 401) {
+                    //not logged in (didn't attach token in the header)
+                    alert('please login');
                 }
-                }).catch((err)=>{
-                    if(err.response.status==401){
-                        //not logged in (didn't attach token in the header)
-                        alert('please login');
-                    }
-                })
-            }
+            })
         }
     }
+}
+
 function status(x, i) {
     if (timeslots[i].reserved == true) {
         document.getElementById(x).style.backgroundColor = "red";
@@ -83,15 +89,15 @@ function reserve_choose(i) {
         if (document.getElementsByClassName("Reserve")[i].innerText == 'Reserve') {
             document.getElementsByClassName("Reserve")[i].innerHTML = 'Chosen'
             document.getElementsByClassName("Reserve")[i].style.fontWeight = 'bold';
-            document.getElementsByClassName('Reserve')[i].style.color="#f90"
-            document.getElementsByClassName('Reserve')[i].style.backgroundColor="black"
+            document.getElementsByClassName('Reserve')[i].style.color = "#f90"
+            document.getElementsByClassName('Reserve')[i].style.backgroundColor = "black"
             a[i] = 'chosen'
         } else {
             if (document.getElementsByClassName("Reserve")[i].innerText == 'Chosen') {
                 document.getElementsByClassName("Reserve")[i].innerHTML = 'Reserve'
                 document.getElementsByClassName("Reserve")[i].style.fontWeight = 'bold';
-                document.getElementsByClassName('Reserve')[i].style.color="black"
-                document.getElementsByClassName('Reserve')[i].style.backgroundColor="#f90"
+                document.getElementsByClassName('Reserve')[i].style.color = "black"
+                document.getElementsByClassName('Reserve')[i].style.backgroundColor = "#f90"
                 a[i] = false
             }
         }
@@ -99,45 +105,61 @@ function reserve_choose(i) {
         alert('This time has been reserve')
     }
 }
+
 function submit() {
-    if (confirm("confirm to submit") == true){
-            var datasend=[];
-            for (var i = 0; i < 20; i++) {
-                if(a[i] == 'chosen'){
-                    datasend.push({reserver:getCookie('user'),room: "testRoom",date: "testDate",slot: i.toString(10)})
+    var y = document.getElementById("date").value;
+    if (confirm("confirm to submit") == true) {
+        var datasend = [];
+        for (var i = 0; i < 20; i++) {
+            if (a[i] == 'chosen') {
+                datasend.push({ reserver: getCookie('user'), room: document.getElementById("Room"), date: y, slot: i.toString(10) })
+            }
+        }
+        if (datasend.length == 0) {
+            alert("Please select")
+        } else {
+            axios({
+                method: 'post',
+                url: 'https://api.pattanachai.xyz/reserve',
+                headers: {
+                    Authorization: getCookie(getCookie('user'))
+                },
+                data: datasend,
+            }).then((response) => {
+                if (response.data.success == true) {
+                    alert('reserve successfully');
+                    refresh(y, document.getElementById("Room"));
+                } else {
+                    alert("can't reserve");
+
                 }
-            }
-            if (datasend.length==0){
-                alert("Please select")
-            }else{
-                axios({
-                    method: 'post', 
-                    url: 'https://api.pattanachai.xyz/reserve',
-                    headers: {
-                        Authorization: getCookie(getCookie('user'))
-                    },
-                    data: datasend,
-                    }).then((response)=>{
-                        if(response.data.success==true){
-                            alert('reserve successfully');
-                            refresh();
-                        }
-                        else{
-                            alert("can't reserve");
-                        
-                        }
-                    }).catch((err)=>{
-                        if(err.response.status==401){
-                            //not logged in (didn't attach token in the header)
-                            alert('please login');
-                            location.replace('login.html');
-                        }
-                    })
-            }
+            }).catch((err) => {
+                if (err.response.status == 401) {
+                    //not logged in (didn't attach token in the header)
+                    alert('please login');
+                    location.replace('login.html');
+                }
+            })
+        }
     }
+}
+
+function getdate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+    a = today
+    return a
+}
+
+function reload() {
+    refresh(document.getElementById('date').value);
 }
 var a = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 var id_status = ["index0_can", "index1_can", "index2_can", "index3_can", "index4_can", "index5_can", "index6_can", "index7_can", "index8_can", "index9_can",
     "index10_can", "index11_can", "index12_can", "index13_can", "index14_can", "index15_can", "index16_can", "index17_can", "index18_can", "index19_can"
 ];
-refresh();
+refresh(getdate(), 'Room1');
+document.getElementById("date").value = getdate();
--- sign_in.js
+++ sign_in.js
@@ -1,23 +1,23 @@
-function submit(){
-    var name = document.getElementById('1').value;
+function submit() {
+    var username = document.getElementById('1').value;
     var password = document.getElementById('2').value;
     var password_comfirm = document.getElementById('3').value;
-    if (password==password_comfirm){
+    var name = document.getElementById('4').value;
+    if (password == password_comfirm) {
         axios({
-            method: 'post', 
+            method: 'post',
             url: 'https://api.pattanachai.xyz/signup',
-            data: {username:name,password:password},
-          }).then((response)=>{
-                if(response.data.success){
-                    alert('Success')
-                }
-                else{
-                    alert('This username has already been taken')
-                }
-          }).catch((err)=>{
-                console.log(err.toString());
-          })
-    }else {
+            data: { username: username, password: password, name: name },
+        }).then((response) => {
+            if (response.data.success) {
+                alert('Success')
+            } else {
+                alert('This username has already been taken')
+            }
+        }).catch((err) => {
+            console.log(err.toString());
+        })
+    } else {
         alert('password comfirm does not match')
     }
 }
