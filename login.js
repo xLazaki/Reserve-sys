b887db34ea55f369936aeb2da4ccbf4d5c4b0191
xLazaki <boss_12389@hotmail.com>
2019-10-30 13:11:13 +0700

    login

--- login.js
+++ login.js
@@ -1,28 +1,32 @@
-function setCookie(cname,cvalue,exdays) {
+function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-function ping(){
-    alert("Please login")
 }
+
 function checklog() {
     var username = document.getElementsByClassName("input_username")[0].value;
     var password = document.getElementsByClassName("input_password")[0].value;
     axios({
-        method: 'post', 
+        method: 'post',
         url: 'https://api.pattanachai.xyz/login',
-        data: {username:username,password:password},
-      }).then((response)=>{
-            if (response.data.success == true) {
+        data: { username: username, password: password },
+    }).then((response) => {
+        if (response.data.success == true) {
+            if (getCookie('user') == 'admin@mail.com') {
                 location.replace('admin.html');
-                setCookie(username,response.data.token,1);
-                setCookie('user',username,1);
+                setCookie(username, response.data.token, 1);
+                setCookie('user', username, 1);
             } else {
-                alert("Wrong username or password");
+                location.replace('user_timeslot.html');
+                setCookie(username, response.data.token, 1);
+                setCookie('user', username, 1);
             }
-      }).catch((err)=>{
-            console.log(err.toString());
-      })
+        } else {
+            alert("Wrong username or password");
+        }
+    }).catch((err) => {
+        console.log(err.toString());
+    })
 }
