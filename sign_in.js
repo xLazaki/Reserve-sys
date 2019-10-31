cfa3c337aa6af5a81965d3477d72c6b747963d77
xLazaki <boss_12389@hotmail.com>
2019-10-31 16:41:17 +0700

    login

--- sign_in.js
+++ sign_in.js
@@ -3,21 +3,34 @@ function sign_up_submit() {
     var password = document.getElementById('2').value;
     var password_comfirm = document.getElementById('3').value;
     var name = document.getElementById('4').value;
-    if (password == password_comfirm) {
-        axios({
-            method: 'post',
-            url: 'https://api.pattanachai.xyz/signup',
-            data: { username: username, password: password, name: name },
-        }).then((response) => {
-            if (response.data.success) {
-                alert('Success')
-            } else {
-                alert('This username has already been taken')
+    if (name.length==0){
+        alert('Empty Name')
+    }else{
+        if(username.length==0){
+            alert('Empty Username')
+        }else{
+            if(password==0 || password_comfirm==0){
+                alert('Empty Password')
+            }else{
+                if (password == password_comfirm) {
+                    axios({
+                        method: 'post',
+                        url: 'https://api.pattanachai.xyz/signup',
+                        data: { username: username, password: password, name: name },
+                    }).then((response) => {
+                        if (response.data.success) {
+                            alert('Welcome'+name)
+                            location.replace('login.html')
+                        } else {
+                            alert('This username has already been taken')
+                        }
+                    }).catch((err) => {
+                        console.log(err.toString());
+                    })
+                } else {
+                    alert('password comfirm does not match')
+                }
             }
-        }).catch((err) => {
-            console.log(err.toString());
-        })
-    } else {
-        alert('password comfirm does not match')
+        }
     }
 }
